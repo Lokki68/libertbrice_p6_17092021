7a2cb9fbea4b0a6912e8b3ff4d8c50239c717d4b
Brice <bricel67@gmail.com>
2021-10-22 09:23:47 +0200

    correction server.js

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,6 @@ const http = require('http');
 const app = require('./app');
 require('dotenv').config({ path: './config/.env' });
 
-const normalizePort = (val) => {
-  const port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
-};
-
 const server = http.createServer(app);
 
 server.listen(process.env.PORT, () => {
